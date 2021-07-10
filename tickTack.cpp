44114c2f9d2f74ef8ef6a888c7938c6d01147971
project <brettendrakgoale1@gmai.com>
2021-07-10 15:16:20 +0200

    update

--- tickTack.cpp
+++ tickTack.cpp
@@ -78,3 +78,33 @@ int main()
     cin.get();
     return 0;
 }
+
+
+/*******************************************************************
+     FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+********************************************************************/
+
+void board()
+{
+    system("cls");
+    cout << "\n\n\tTic Tac Toe\n\n";
+
+    cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+    cout << endl;
+
+    cout << "|     |     |     |" << endl;
+    cout << "|  " << square[1] <<"  |  "<< square[2] << "  |  "<< square[3] <<"  |"<< endl;
+
+    cout << "|_____|_____|_____|" << endl;
+    cout << "|     |     |     |" << endl;
+
+    cout << "|  " << square[4] <<"  |  " << square[5] << "  |  " << square[6] <<"  |"<< endl;
+
+    cout << "|_____|_____|_____|" << endl;
+    cout << "|     |     |     |" << endl;
+
+    cout << "|  " << square[7] << "  |  " << square[8] << "  |  " << square[9] <<"  |"<< endl;
+
+    cout << "|     |     |     |" << endl << endl;
+}
+//end of project
