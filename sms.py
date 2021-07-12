ccee34c7ad54df337973a92f93826bd26096d236
Brettend <87177774+Brettend@users.noreply.github.com>
2021-07-12 14:54:31 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,48 +1,69 @@
-# this is the sms project
-#class definition starts below
+# SMS project
 
+SMSStore = []
+#class definition starts below
 class SMSMessage:
-    hasBeenRead=False
-    messageText= txt
-    fromNumber= number
+
     def __init__(self, hasBeenRead, messageText, fromNumber):
     #initialization of  the instance variables
+        global SMSStore
         self.hasBeenRead = hasBeenRead
         self.messageText = messageText
         self.fromNumber = fromNumber
 
     def mark_as_read(self):
-        if userChoice == read:
-            self.hasBeenRead = True
+        self.hasBeenRead = bool(True)
 
-    #instance get method
-    def get_smsmessage(self):
-        return f'{self.hasBeenRead}.{self.messageText}.{self.fromNumber}'
+#methods being implemented
+    def add_sms(self, smses):
+        for number in range(smses):
+            source_number = int(input("please enter source number..."))
+            message_body = str(input("please enter message body..."))
+            new_message = (source_number, message_body)
+            SMSStore.append(new_message)
+            return print(SMSStore)
+#get methods implementations
+    def get_count(self):
+        return print(SMSMessage.count())
 
-    #creating objects for this Sms class / call the method
-    no_1 = SMSMessage(False, "Hello", "0643431281")
-    print(no_1.get_smsmessage())
-    no_2 = SMSMessage(False, "WYD", "0604204078")
-    print(no_2.get_smsmessage())
-    no_3 = SMSMessage(False, "how are you?", "0714171229")
-    print(no_3.get_smsmessage())
+    def get_message(self):
+        return print(len(SMSStore))
+
+    def get_unread_messages(self):
+         pass
 
 
-SMSStore=[]
-userChoice=""
+no_1 = SMSMessage(False, "Hello", "0643431281")
+print(no_1.get_smsmessage())
+no_2 = SMSMessage(False, "WYD", "0604204078")
+print(no_2.get_smsmessage())
+no_3 = SMSMessage(False, "how are you?", "0714171229")
+print(no_3.get_smsmessage())
 
-    while userChoice != "quit":
-        userChoice = raw_input("what would you like to do -read/send/quit?")
+
+
+userChoice="None"
+
+while userChoice != "quit":
+        userChoice = input("what would you like to do -read/send/quit?")
         if userChoice == "read":
-            messageText.hasBeenRead=True
-       # elif userChoice=="send":
-         #statements
-        #elif userChoice=="quit":
-            #statements
+            SMSStore.get_count()
+        elif userChoice=="send":
+            smses = int(input("number of smses to be added"))
+            SMSMessage.add_sms(smses)
+        #exit the prompt if option says so
+        elif userChoice=="quit":
+         print("Goodbye")
+        else:
+                print("oops - wrong input")
 
-    print("Goodbye")
-    #else:
-    print("oops - wrong input")
+
+
+
+
+
+
+
 
 
 
